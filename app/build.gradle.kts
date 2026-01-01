640d2bfd6a6d46ffd32d56e4f286686a0f03458f
José Luis Medina Carretero <joseluismc81@gmail.com>
2026-01-01 01:05:42 +0100

    Bump version to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.iatrading.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
